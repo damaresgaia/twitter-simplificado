b8a5825f27b4dc5f66a5c23f3c3f3196bc01a194
Damares Gaia <damaresgaia@gmail.com>
2024-11-17 21:39:01 -0300

    Adiciona pacote entities

---
+++ src/main/java/twitter/simplificado/springsecurity/entities/Tweet.java
@@ -0,0 +1,28 @@
+package twitter.simplificado.springsecurity.entities;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_tweets")
+public class Tweet {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE)
+	private Long tweetId;
+	
+	private User user;
+	
+	private String content;
+	
+	@CreationTimestamp
+	private Instant creationTimestamp;
+
+}
