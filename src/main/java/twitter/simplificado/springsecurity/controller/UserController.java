3e84ae4179dad6693b0a0402616f3c36b0bda127
Damares Gaia <damaresgaia@gmail.com>
2024-11-20 17:10:33 -0300

    Adiciona funcionalidade de criação de novo usuário

---
+++ src/main/java/twitter/simplificado/springsecurity/controller/UserController.java
@@ -0,0 +1,58 @@
+package twitter.simplificado.springsecurity.controller;
+
+import java.util.Set;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import twitter.simplificado.springsecurity.controller.dto.CreateUserDto;
+import twitter.simplificado.springsecurity.entities.Role;
+import twitter.simplificado.springsecurity.entities.User;
+import twitter.simplificado.springsecurity.repository.RoleRepository;
+import twitter.simplificado.springsecurity.repository.UserRepository;
+
+@RestController
+public class UserController {
+	
+	private final UserRepository userRepository;
+	
+	private final RoleRepository roleRepository; 
+	
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	public UserController(UserRepository userRepository,
+					      RoleRepository roleRepository,
+					      BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.roleRepository = roleRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+	
+	@Transactional
+	@PostMapping("/users")
+	public ResponseEntity<Void> newUser(@RequestBody CreateUserDto dto) {
+		
+		var basicRole = roleRepository.findByName(Role.Values.BASIC.name());
+		
+		var userFromDb = userRepository.findByUsername(dto.username());
+		if (userFromDb.isPresent()) {
+			throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+		}
+		
+		var user = new User();
+		user.setUsername(dto.username());
+		user.setPassword(bCryptPasswordEncoder.encode(dto.password()));
+		user.setRoles(Set.of(basicRole));
+		
+		userRepository.save(user);		
+		
+		return ResponseEntity.ok().build();
+	}
+
+}
---
+++ src/main/java/twitter/simplificado/springsecurity/controller/dto/CreateUserDto.java
@@ -0,0 +1,5 @@
+package twitter.simplificado.springsecurity.controller.dto;
+
+public record CreateUserDto(String username, String password) {
+
+}
